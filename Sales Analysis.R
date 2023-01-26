91170922f3ce49de36a775d7b30990b218440c6d
Steven Garcia <116322698+stevengarcia2132@users.noreply.github.com>
2023-01-25 22:43:48 -0700

    Update Sales Analysis.R

--- Sales Analysis.R
+++ Sales Analysis.R
@@ -3,80 +3,156 @@ library(tidyr)
 library(ggplot2)
 library(lubridate)
 library(stringr)
-sales <- read.csv("~/2022 SCHOOL/FALL 2022/Sales2022.csv")
+library(readxl)
+library(knitr)
+library(scales)
 
-sales <- select(sales, -c(Balance,Last.Name,Phone.No., Credit.Balance,
-                          PL,Sales.External.Id,Marketing.Type,Ext..Account.Number))
-sales <- as_tibble(sales)
+```{r Data importing and cleaning, include=FALSE}
 
-sales$Account.Start.Date <- mdy(sales$Account.Start.Date)
-sales$Contract.Creation.Date <- mdy(sales$Contract.Creation.Date)
-sales$Initial.Date <- mdy(sales$Initial.Date)
+compSales <- read_xlsx("~/2022 SCHOOL/FALL 2022/CompanySales.xlsx")
 
-cols <- c("Customer.Status","Sales.Status", "Autopay","Contract.Name")
-sales[cols] <- lapply(sales[cols],factor)
-head(sales)
+compSales <- as_tibble(compSales)
 
 
-#Data Visualization
-#total sales by month
-sales %>%
-  filter(Customer.Status == "Active") %>%
-  ggplot(aes(x = month(Account.Start.Date))) + geom_bar(fill = "brown")+
-  geom_text(stat='count',aes(label=..count..),vjust=-1)+
+
+cols <- c("State" ,"Customer Status","Sales Status", "Autopay","Contract Name",
+          "Primary Service Type", "Marketing Type", "Service Frequency", "Sales Rep")
+compSales[cols] <- lapply(compSales[cols],factor)
+```
+
+# Pest Control Sales Compared to Lawn Care Sales
+```{r Lawn care and pest sales, echo=FALSE}
+LawnPest <- compSales %>%
+  select(`Primary Service Type`) %>%
+  group_by(`Primary Service Type`) %>%
+  mutate(Sales = n()) %>%
+  distinct(`Primary Service Type`, .keep_all = TRUE)
+
+revenueLawn <- compSales %>%
+  filter(`Primary Service Type` == "Lawn/Weed") %>%
+  summarise(Total_Revenue = sum(`Contract Value`))
+
+revenuePest <- compSales %>%
+    filter(`Primary Service Type` == "Comprehensive Residential Treatment") %>%
+    summarise(Total_Revenue = sum(`Contract Value`))
+
+
+totalRevenue <- data.frame(ServiceType = c("Lawn/Weed", "Comprehensive Pest Control"),
+                           Revenue = c(115071,327372))
+options(scipen = 999)
+ggplot(data = totalRevenue)+
+  geom_col(aes(x = ServiceType, y = Revenue, fill = ServiceType),show.legend = F)+
+  scale_y_continuous(label=comma, limits=c(min(0),max(400000)))+ 
+  geom_text(aes(x = ServiceType, y = Revenue, label= comma(Revenue)),vjust=-1)+
   theme(aspect.ratio = 1)+
-  labs(x = "Months" , y= "Accounts created", title = "Accounts Created by \n month for Steven")+
-  scale_x_continuous(breaks = c(6,7,8),labels=c("June","July","August"))+
-  ylim(0,70)
-  
-  
-#total sales by day of week
-sales %>%
-  filter(Customer.Status == "Active") %>%
-  ggplot(aes(x=weekdays(Account.Start.Date))) + geom_bar(fill= "brown")+
-  xlim("Monday", "Tuesday","Wednesday", "Thursday", "Friday","Saturday")+
+  labs(title = "Revenue of Pest Control and Lawn Care")
+``` 
+
+# Highest overall contract value by salesman
+```{r Analysis, echo=FALSE, message=FALSE, warning=FALSE}
+HighestContract <- compSales %>%
+  group_by(`Sales Rep`) %>%
+  select(`Contract Value`) %>%
+  arrange(desc(`Contract Value`)) %>%
+  distinct(`Sales Rep`, .keep_all = TRUE) %>%
+  ungroup()
+
+colnames(HighestContract) <- c("Salesman","Contract Value") 
+
+kable(HighestContract)
+
+```
+
+## Total Accounts Created
+```{r Accounts created by salesman, echo=FALSE}
+ggplot(data = compSales) +
+  geom_bar(aes(x = `Sales Rep`, color = `Sales Rep`, fill = `Sales Rep`),
+           show.legend = FALSE) +
   theme(axis.text.x = element_text(angle = 90))+
-  ylim(0,40)+
-  geom_text(stat='count',aes(label=..count..),vjust=-1)+
-  labs(x = "Day of week", y = "Accounts created", title = "Accounts created by \n day of week")
+  labs(x = "Salesman", y = "Accounts Created")+
+  geom_text(stat='count',aes(x = `Sales Rep`, label=..count..),vjust=-1)+
+  ylim(0,600)
+
+```
+
+## Revenue Created by Salesman
+```{r Revenue created by Saleman, echo=FALSE}
+revenueSalesman <- compSales %>%
+  group_by(`Sales Rep`) %>%
+  summarise(Revenue = sum(`Contract Value`)) %>%
+  ungroup()
   
 
+ggplot(data = revenueSalesman) +
+  geom_col(aes(x= `Sales Rep`, y = Revenue, fill = `Sales Rep`),show.legend = F)+
+  theme(axis.text.x = element_text(angle = 90))+
+  geom_text(aes(x = `Sales Rep`, y = Revenue, label= comma(Revenue)),vjust=-1)+
+  scale_y_continuous(label=comma, limits=c(min(0),max(200000)))
+
+```
+
+## Comparison of Contract Values
+```{r avg contract value, echo=FALSE}
+avgCV <- compSales %>%
+  group_by(`Sales Rep`) %>%
+  summarise(Revenue = mean(`Contract Value`)) %>%
+  ungroup
 
 
-#creating a function to detect if the input city address
-cityDetect <- function(x){
-  count <- 0
-  for (i in 1:length(sales$Address)) {
-    
-    if(str_detect(sales$Address[i], x) == TRUE){
-    count <- count + 1
-    }
-  }
-  count
-}
+avgPest <- compSales %>%
+  group_by(`Sales Rep`) %>%
+  filter(`Primary Service Type` == "Comprehensive Residential Treatment") %>%
+  summarise(Revenue = mean(`Contract Value`)) %>%
+  ungroup
 
-EagleSales <- cityDetect("Eagle Mountain")
-LehiSales <- cityDetect("Lehi")
-DraperSales <- cityDetect("Draper")
-SaratogaSales <- cityDetect("Saratoga")
+avgLawn <- compSales %>%
+  group_by(`Sales Rep`) %>%
+  filter(`Primary Service Type` == "Lawn/Weed") %>%
+  summarise(Revenue = mean(`Contract Value`)) %>%
+  ungroup
 
+ggplot(data = avgCV) +
+  geom_col(aes(x= `Sales Rep`, y = Revenue, fill = `Sales Rep`),show.legend = F)+
+  theme(axis.text.x = element_text(angle = 90))+
+  geom_text(aes(x = `Sales Rep`, y = Revenue, label= comma(Revenue)),vjust=-1)+
+  ylim(0,600)+
+  labs(title = "Average Contract Value")
 
-EagleSales <- NA
-citySales <- sales %>%
-  filter(Customer.Status == "Active") %>%
-  for (i in 1:nrow(Address)){
-    if(str_detect(Address[i]) == "Eagle Mountain"){
-      EagleSales <- EagleSales + 1
-    }
-  }
+mean(avgCV)
 
 
+ggplot(data = avgPest) +
+  geom_col(aes(x= `Sales Rep`, y = Revenue, fill = `Sales Rep`),show.legend = F)+
+  theme(axis.text.x = element_text(angle = 90))+
+  geom_text(aes(x = `Sales Rep`, y = Revenue, label= comma(Revenue)),vjust=-1)+
+  ylim(0,600)+
+  labs(title = "Average Pest Control Contract Value")
 
-#City sales
-sales %>%
-  filter(Customer.Status == "Active") %>%
-  ggplot(aes(x = ))
-  str_detect()
-head(sales)
+ggplot(data = avgLawn) +
+  geom_col(aes(x= `Sales Rep`, y = Revenue, fill = `Sales Rep`),show.legend = F)+
+  theme(axis.text.x = element_text(angle = 90))+
+  geom_text(aes(x = `Sales Rep`, y = Revenue, label= comma(Revenue)),vjust=-1)+
+  ylim(0,600)+
+  labs(title = "Average Lawn Care Contract Value")
+
+```
+##Sales by day of Week
+```{r dates}
+
+daysofWeek <- compSales %>%
+  summarize(day = weekdays(`Contract Creation Date`))
+
+monday <- daysofWeek %>%
+  filter(day == "Monday")
+nrow(monday)
+
+
+ggplot(data = compSales)+
+  geom_bar(mapping = aes(x= weekdays(`Contract Creation Date`),fill = weekdays(`Contract Creation Date`)),show.legend = F) + 
+geom_text(stat='count',aes(x = weekdays(`Contract Creation Date`), label=..count..),vjust=-1)+
+  ylim(0,350)+
+  labs(title ="Sales by Day of Week")+
+  xlab("Days of Week")
+
 
 
